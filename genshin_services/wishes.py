942c09ad9eb08d6b0ba5173ecac996dec3b4ec28
lebelius <alexeevroma@gmail.com>
2022-05-29 21:34:20 +0300

    add auth fucntion

--- genshin_services/wishes.py
+++ genshin_services/wishes.py
@@ -2,10 +2,12 @@ from genshin import Client
 
 from tg_bot.config import load_config
 
-if __name__ == '__main__':
+def get_client():
     config = load_config("../bot.ini")
     client = Client()
     client.set_cookies(ltoken=config.genshin.ltoken, ltuid=config.genshin.ltuid)
+
+
 
 
 
