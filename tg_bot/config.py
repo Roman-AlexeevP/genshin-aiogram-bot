b43b04c1e01905487fb599313e9564d4afdf1765
lebelius <alexeevroma@gmail.com>
2022-05-29 21:34:12 +0300

    fix config import

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -37,4 +37,4 @@ def load_config(path: str):
         tg_bot=TelegramBot(
             token=tg_bot["token"],
         ),
-        genshin=GenshinAPI(**config["genshin_services"]))
+        genshin=GenshinAPI(**config["genshin"]))
