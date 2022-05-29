de1446b7a011ea91cfb8bab829874278864d13fc
lebelius <alexeevroma@gmail.com>
2022-05-29 21:15:57 +0300

    init commit

--- genshin-aiogram-bot/tg_bot/handlers/genshin.py
+++ genshin-aiogram-bot/tg_bot/handlers/genshin.py
@@ -3,12 +3,13 @@ import logging
 from aiogram import Dispatcher
 from aiogram.types import Message
 
+
 logger = logging.getLogger(__name__)
 
-async def user_start(m: Message):
-    await m.answer("Hello, user!")
+# write commands handlers here
 
 
 def register_genshin(dp: Dispatcher):
-    dp.register_message_handler(user_start, commands="test")
+    # dp.register_message_handler()
+    # register commands here
     logger.info("genshin handlers registered commands successfuly")
